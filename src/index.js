7aeadafc98c0d02cb093e3de00c07e7e1e11c757
GitHub Action <github@reedsy.com>
2025-07-09 15:55:22 +0200

    🐛 Fix ESM webSocket import issue
    We are getting this error when using in ESM enviroment:
    ```
    TypeError: ws.Server is not a constructor
        at expressWs (file:///Users/dawidkisielewski/coding/reedsy/reedsy-editor/node_modules/@reedsy/express-ws/src/index.js:44:20)
        at EditorServer.startWebSocketServer (file:///Users/dawidkisielewski/coding/reedsy/reedsy-editor/packages/server/build/editor-server.js:146:25)
        at EditorServer.start (file:///Users/dawidkisielewski/coding/reedsy/reedsy-editor/packages/server/build/editor-server.js:43:20)
        at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
    
    ```

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 
 import http from 'http';
 import express from 'express';
-import ws from 'ws';
+import { WebSocketServer } from 'ws';
 
 import websocketUrl from './websocket-url.js';
 import addWsMethod from './add-ws-method.js';
@@ -40,7 +40,7 @@ export default function expressWs(app, httpServer, options = {}) {
   // allow caller to pass in options to WebSocketServer constructor
   const wsOptions = options.wsOptions || {};
   wsOptions.server = server;
-  const wsServer = new ws.Server(wsOptions);
+  const wsServer = new WebSocketServer(wsOptions);
 
   wsServer.on('connection', (socket, request) => {
     if ('upgradeReq' in socket) {
